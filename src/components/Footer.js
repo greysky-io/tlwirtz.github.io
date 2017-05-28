58cb02758ef2c04ea48936d94e38de2454121460
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-05-28 11:00:42 -0700

    footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SectionContent from './SectionContent';
+
+const Footer = props => {
+  return (
+    <div className="app-container-item main-background">
+      <SectionContent>
+        </SectionContent>
+    </div>
+  );
+};
+
+Footer.propTypes = {};
+
+export default Footer;
