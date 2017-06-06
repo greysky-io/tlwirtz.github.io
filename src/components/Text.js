eb7beebcce8bda1deb4b075d53b56a718e68c5c9
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2017-06-06 13:48:52 -0700

    new text componetn

---
+++ src/components/Text.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+const Text = props => {
+  const {
+    title,
+    heading,
+    subheading,
+    body,
+    color,
+    weight,
+    position,
+    bottomMargin,
+    infoCard,
+    children,
+  } = props;
+
+  const fontWeight = weight ? `text-${weight}` : 'text-light';
+  const fontColor = color ? `text-${color}` : 'text-dark';
+  const extraMargin = bottomMargin ? 'text-margin-bottom' : null;
+  const textClasses = classnames(
+    {
+      'text-body': body,
+      'text-headline': heading,
+      'text-subheading': subheading,
+      'text-title': title,
+      'info-card-item': infoCard
+    },
+    fontWeight,
+    fontColor,
+    extraMargin,
+    position
+  );
+
+  const buildComponent = classes => {
+    if (heading || title) {
+      return <h1 className={classes}>{children}</h1>;
+    }
+
+    if (subheading) {
+      return <h2 className={classes}>{children}</h2>;
+    }
+
+    return <p className={classes}>{children}</p>;
+  };
+
+  return buildComponent(textClasses);
+};
+
+Text.propTypes = {};
+
+export default Text;
