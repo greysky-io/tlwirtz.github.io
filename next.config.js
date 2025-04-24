4d379ad21930826240ce112bb116eaa1a7a50866
Taylor Wirtz <taylor.wirtz@gmail.com>
2025-04-23 20:20:56 -0700

    trying to upgrade to latest next

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+const { default: styled } = require("styled-components");
+
 // next.config.js
 module.exports = {
-  exportPathMap: function(defaultPathMap) {
+  exportPathMap: function (defaultPathMap) {
     return {
       '/': { page: '/' },
     };
   },
+  compiler: {
+    styledComponents: true,
+  }
 };
